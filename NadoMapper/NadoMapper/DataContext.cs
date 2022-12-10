bc0856558102490234dd4eb2164d0beafbb3979f
Nathan <67047470+nathnado22@users.noreply.github.com>
2022-12-10 12:20:31 +1100

    Working on ExecuteNonQuery, ExecuteScalar, ExecuteReader

--- NadoMapper/NadoMapper/DataContext.cs
+++ NadoMapper/NadoMapper/DataContext.cs
@@ -9,115 +9,149 @@ using Pluralize.NET;
 
 namespace NadoMapper
 {
+  /// <summary>
+  /// A generalised, standalone wrapper which gives the ability to perform basic CRUD operations on a database.
+  /// The DataContext may be included as a member of a parent "Repository" class to provide specific functionality.
+  /// </summary>
+  /// <typeparam name="TEntity"></typeparam>
+  public class DataContext<TEntity> where TEntity : ModelBase, new()
+  {
+    private readonly SqlProviderAsync _sqlProviderAsync;
+
+    public List<PropertyConventionBase> PropertyConventions => _sqlProviderAsync.PropertyConventions;
+
+    private string modelName => typeof(TEntity).Name;
+    private string modelNamePlural { get; }
+
     /// <summary>
-    /// A generalised, standalone wrapper which gives the ability to perform basic CRUD operations on a database.
-    /// The DataContext may be included as a member of a parent "Repository" class to provide specific functionality.
+    /// Create a new DataContext accepting a <paramref name="connectionString"/> for use with models of type <paramref name="TEntity"/>
     /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    public class DataContext<TEntity> where TEntity: ModelBase, new()
+    /// <param name="connectionString"></param>
+    public DataContext(string connectionString)
     {
-        private readonly SqlProviderAsync _sqlProviderAsync;
+      var pluralizer = new Pluralizer();
+      modelNamePlural = pluralizer.Pluralize(modelName);
 
-        public List<PropertyConventionBase> PropertyConventions => _sqlProviderAsync.PropertyConventions;
+      _sqlProviderAsync = new SqlProviderAsync(connectionString);
+    }
 
-        private string modelName => typeof(TEntity).Name;
-        private string modelNamePlural { get; }
+    /// <summary>
+    /// Execute a stored procedure by given name, and return the number of rows updated
+    /// </summary>>
+    public Task<long> ExecuteNonQueryAsync(string command, CRUDType crudType, Dictionary<string, object> parameters = null)
+     => _sqlProviderAsync.ExecuteNonQueryAsync(command, crudType, parameters);
 
-        /// <summary>
-        /// Create a new DataContext accepting a <paramref name="connectionString"/> for use with models of type <paramref name="TEntity"/>
-        /// </summary>
-        /// <param name="connectionString"></param>
-        public DataContext(string connectionString)
-        {
-            var pluralizer = new Pluralizer();
-            modelNamePlural = pluralizer.Pluralize(modelName);
+    /// <summary>
+    /// Execute a stored procedure by given name, and return an object of type <paramref name="TEntity"/>
+    /// which satisfies the given parameter
+    /// </summary>>
+    public Task<object> ExecuteScalarAsync(string command, CRUDType crudType, string parameterName, object parameterValue)
+        => ExecuteScalarAsync(command, crudType, parameterName, parameterValue);
 
-            _sqlProviderAsync = new SqlProviderAsync(connectionString);
-        }
+    /// <summary>
+    /// Execute a stored procedure by given name, and return an object of type <paramref name="TEntity"/>
+    /// which satisfies the given parameters
+    /// </summary>>
+    public Task<object> ExecuteScalarAsync(string command, CRUDType crudType, Dictionary<string, object> parameters = null)
+        => _sqlProviderAsync.ExecuteScalarAsync(command, crudType, parameters);
 
-        /// <summary>
-        /// Retrieve all of the rows in the database for a given model.
-        /// Note: Requires stored procedure "Get[modelName](s/es)" e.g. "GetTest(s)".
-        /// </summary>>
-        /// <param name="id"></param>
-        /// <returns>All entities of type <paramref name="TEntity"/></returns>
-        public async Task<IEnumerable<TEntity>> GetAllAsync()
-        {
-            var data = await _sqlProviderAsync.ExecuteReaderAsync($"Get{modelNamePlural}");
+    /// <summary>
+    /// Execute a stored procedure by given name, and return a collection of objects of type <paramref name="TEntity"/>
+    /// which satisfies the given parameter
+    /// </summary>>
+    public Task<IEnumerable<Dictionary<string, object>>> ExecuteReaderAsync(string command, string parameterName, object parameterValue)
+        => ExecuteReaderAsync(command, new Dictionary<string, object>() { { parameterName, parameterValue } });
 
-            return data.Select(d => NadoMapper.MapPropsToSingle<TEntity>(d));
-        }
+    /// <summary>
+    /// Execute a stored procedure by given name, and return a collection of objects of type <paramref name="TEntity"/>
+    /// which satisfies the given parameters
+    /// </summary>>
+    public Task<IEnumerable<Dictionary<string, object>>> ExecuteReaderAsync(string command, Dictionary<string, object> parameters = null)
+        => _sqlProviderAsync.ExecuteReaderAsync(command, parameters);
 
-        /// <summary>
-        /// Retrieve a single row from the database by <paramref name="id"/>.
-        /// Note: Requires stored procedure "Get[modelName]ById " e.g. "GetTestById".
-        /// </summary>>
-        /// <param name="id"></param>
-        /// <returns>An entity of type <paramref name="TEntity"/> corresponding to <paramref name="id"/></returns>
-        public async Task<TEntity> GetSingleByIdAsync(long id) =>
-            await GetSingleAsync("id",id);
+    /// <summary>
+    /// Retrieve all of the rows in the database for a given model.
+    /// Note: Requires stored procedure "Get[modelName](s/es)" e.g. "GetTest(s)".
+    /// </summary>>
+    /// <param name="id"></param>
+    /// <returns>All entities of type <paramref name="TEntity"/></returns>
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+      var data = await _sqlProviderAsync.ExecuteReaderAsync($"Get{modelNamePlural}");
 
-        /// <summary>
-        /// Retrieve a single row from the database by <paramref name="name"/>.
-        /// Note: Requires stored procedure "Get[modelName]ByName " e.g. "GetTestByName".
-        /// </summary>>
-        /// <param name="name"></param>
-        /// <returns>An entity of type <paramref name="TEntity"/> corresponding to <paramref name="name"/></returns>
-        public async Task<TEntity> GetSingleByNameAsync(string name) =>
-            await GetSingleAsync("name", name);
+      return data.Select(d => NadoMapper.MapPropsToSingle<TEntity>(d));
+    }
 
-        /// <summary>
-        /// Retrieve a single row from the database based on some parameter (<paramref name="parameterName"/> and <paramref name="parameterValue"/>)
-        /// Note: Requires stored procedure "Get[modelName]By[parameterName]" e.g. "GetTestByName".
-        /// </summary>>
-        /// <param name="parameterName"></param>
-        /// <param name="parameterValue"></param>
-        /// <returns>An entity of type <paramref name="TEntity"/> corresponding to <paramref name="parameterName"/></returns>
-        public async Task<TEntity> GetSingleAsync(string parameterName, object parameterValue)
-        {
-            var procName = $"Get{modelName}By{parameterName.ToUpper()[0] + parameterName.Substring(1)}";
+    /// <summary>
+    /// Retrieve a single row from the database by <paramref name="id"/>.
+    /// Note: Requires stored procedure "Get[modelName]ById " e.g. "GetTestById".
+    /// </summary>>
+    /// <param name="id"></param>
+    /// <returns>An entity of type <paramref name="TEntity"/> corresponding to <paramref name="id"/></returns>
+    public async Task<TEntity> GetSingleByIdAsync(long id) =>
+        await GetSingleAsync("id", id);
 
-            var data = await _sqlProviderAsync.ExecuteReaderAsync(procName, parameterName, parameterValue);
-            var single = data.FirstOrDefault();
+    /// <summary>
+    /// Retrieve a single row from the database by <paramref name="name"/>.
+    /// Note: Requires stored procedure "Get[modelName]ByName " e.g. "GetTestByName".
+    /// </summary>>
+    /// <param name="name"></param>
+    /// <returns>An entity of type <paramref name="TEntity"/> corresponding to <paramref name="name"/></returns>
+    public async Task<TEntity> GetSingleByNameAsync(string name) =>
+        await GetSingleAsync("name", name);
 
-            return NadoMapper.MapSingle<TEntity>(single);
-        }
+    /// <summary>
+    /// Retrieve a single row from the database based on some parameter (<paramref name="parameterName"/> and <paramref name="parameterValue"/>)
+    /// Note: Requires stored procedure "Get[modelName]By[parameterName]" e.g. "GetTestByName".
+    /// </summary>>
+    /// <param name="parameterName"></param>
+    /// <param name="parameterValue"></param>
+    /// <returns>An entity of type <paramref name="TEntity"/> corresponding to <paramref name="parameterName"/></returns>
+    public async Task<TEntity> GetSingleAsync(string parameterName, object parameterValue)
+    {
+      var procName = $"Get{modelName}By{parameterName.ToUpper()[0] + parameterName.Substring(1)}";
 
-        /// <summary>
-        /// Create a new row in the database containing the parameters of <paramref name="model" /> and return it's id.
-        /// Note: Requires stored procedure "Add[modelName]" e.g. "AddTest"
-        /// </summary>
-        /// <param name="model"></param>
-        /// <exception cref="T:System.ArgumentException"/>
-        /// <returns>An id of type <see cref="T:System.Int64"/></returns>
-        public async Task<long> AddAsync(TEntity model)
-        {
-            var parameters = NadoMapper.ReflectPropsFromSingle(model);
-            var id = await _sqlProviderAsync.ExecuteScalarAsync($"Add{modelName}", CRUDType.Create, parameters);
+      var data = await _sqlProviderAsync.ExecuteReaderAsync(procName, parameterName, parameterValue);
+      var single = data.FirstOrDefault();
 
-            return Convert.ToInt64(id);
-        }
+      return NadoMapper.MapSingle<TEntity>(single);
+    }
 
-        /// <summary>
-        /// Update a row (or rows) in the database based on the parameters specified in <paramref name="model" /> and return the number of rows that were updated.
-        /// Note: Requires stored procedure "Update[modelName] e.g. "UpdateTest"
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns>Number of rows updated as a <see cref="T:System.Int64"/></returns>
-        public async Task<long> UpdateAsync(TEntity model) 
-            => await _sqlProviderAsync.ExecuteNonQueryAsync($"Update{modelName}", CRUDType.Update, NadoMapper.ReflectPropsFromSingle(model));
+    /// <summary>
+    /// Create a new row in the database containing the parameters of <paramref name="model" /> and return it's id.
+    /// Note: Requires stored procedure "Add[modelName]" e.g. "AddTest"
+    /// </summary>
+    /// <param name="model"></param>
+    /// <exception cref="T:System.ArgumentException"/>
+    /// <returns>An id of type <see cref="T:System.Int64"/></returns>
+    public async Task<long> AddAsync(TEntity model)
+    {
+      var parameters = NadoMapper.ReflectPropsFromSingle(model);
+      var id = await _sqlProviderAsync.ExecuteScalarAsync($"Add{modelName}", CRUDType.Create, parameters);
 
-        /// <summary>
-        /// Delete a row from the database corresponding to the <paramref name="Id"/> and <paramref name="LastModified"/> of <paramref name="model"/>
-        /// Note: Requires stored procedure "Delete[modelName] e.g. "DeleteTest"
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns>Number of rows updated as a <see cref="T:System.Int64"/></returns>
-        public async Task<long> DeleteAsync(TEntity model)
-        => await _sqlProviderAsync.ExecuteNonQueryAsync($"Delete{modelName}", CRUDType.Update, new Dictionary<string, object>()
-        {
+      return Convert.ToInt64(id);
+    }
+
+    /// <summary>
+    /// Update a row (or rows) in the database based on the parameters specified in <paramref name="model" /> and return the number of rows that were updated.
+    /// Note: Requires stored procedure "Update[modelName] e.g. "UpdateTest"
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns>Number of rows updated as a <see cref="T:System.Int64"/></returns>
+    public async Task<long> UpdateAsync(TEntity model)
+        => await _sqlProviderAsync.ExecuteNonQueryAsync($"Update{modelName}", CRUDType.Update, NadoMapper.ReflectPropsFromSingle(model));
+
+    /// <summary>
+    /// Delete a row from the database corresponding to the <paramref name="Id"/> and <paramref name="LastModified"/> of <paramref name="model"/>
+    /// Note: Requires stored procedure "Delete[modelName] e.g. "DeleteTest"
+    /// </summary>
+    /// <param name="model"></param>
+    /// <returns>Number of rows updated as a <see cref="T:System.Int64"/></returns>
+    public async Task<long> DeleteAsync(TEntity model)
+    => await _sqlProviderAsync.ExecuteNonQueryAsync($"Delete{modelName}", CRUDType.Update, new Dictionary<string, object>()
+    {
             {"id",model.Id},
             {"lastModified",model.LastModified}
-        });
-    }
+    });
+  }
 }
--- NadoMapper/NadoMapper/RepositoryBase.cs
+++ NadoMapper/NadoMapper/RepositoryBase.cs
@@ -1,50 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Security;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Web;
 using NadoMapper.Models;
 using NadoMapper.Conventions;
-using Pluralize;
-using Pluralize.NET;
+using NadoMapper.SqlProvider;
 
 namespace NadoMapper
 {
-    /// <summary>
-    /// Implements the "DataContext" class to expose public methods to perform basic CRUD (Get, Add, Update, Delete) on an SQL Database. This class can be inherited by several repository classes
-    /// in a larger project that have been configured for a particular purpose
-    /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    public class RepositoryBase<TEntity> where TEntity : ModelBase, new()
+  /// <summary>
+  /// Implements the "DataContext" class to expose public methods to perform basic CRUD (Get, Add, Update, Delete) on an SQL Database. This class can be inherited by several repository classes
+  /// in a larger project that have been configured for a particular purpose
+  /// </summary>
+  /// <typeparam name="TEntity"></typeparam>
+  public class RepositoryBase<TEntity> where TEntity : ModelBase, new()
+  {
+    private readonly DataContext<TEntity> _dataContext;
+
+    public List<PropertyConventionBase> PropertyConventions => _dataContext.PropertyConventions;
+
+    public RepositoryBase(string connectionString)
     {
-        private readonly DataContext<TEntity> _dataContext;
+      _dataContext = new DataContext<TEntity>(connectionString);
 
-        public List<PropertyConventionBase> PropertyConventions => _dataContext.PropertyConventions;
-
-        public RepositoryBase(string connectionString)
-        {
-            _dataContext = new DataContext<TEntity>(connectionString);
-
-            _dataContext.PropertyConventions.Add(new IgnoreDateAddedDuringAddPropertyConvention());
-            _dataContext.PropertyConventions.Add(new IgnoreLastModifiedDuringAddPropertyConvention());
-            _dataContext.PropertyConventions.Add(new IgnoreDateAddedDuringUpdatePropertyConvention());
-            _dataContext.PropertyConventions.Add(new IgnoreIdDuringAddPropertyConvention());
-        }
-
-        public Task<IEnumerable<TEntity>> GetAllAsync() => _dataContext.GetAllAsync();
-
-        public Task<TEntity> GetSingleAsync(string parameterName, object parameterValue) => _dataContext.GetSingleAsync(parameterName, parameterValue);
-
-        public Task<TEntity> GetSingleAsync(long id) => _dataContext.GetSingleByIdAsync(id);
-
-        public Task<long> AddAsync(TEntity item) => _dataContext.AddAsync(item);
-
-        public Task<long> UpdateAsync(TEntity item) => _dataContext.UpdateAsync(item);
-
-        public Task<long> DeleteAsync(TEntity item) => _dataContext.DeleteAsync(item);
+      _dataContext.PropertyConventions.Add(new IgnoreDateAddedDuringAddPropertyConvention());
+      _dataContext.PropertyConventions.Add(new IgnoreLastModifiedDuringAddPropertyConvention());
+      _dataContext.PropertyConventions.Add(new IgnoreDateAddedDuringUpdatePropertyConvention());
+      _dataContext.PropertyConventions.Add(new IgnoreIdDuringAddPropertyConvention());
     }
+
+    /// <summary>
+    /// Execute a stored procedure by given name, and return the number of rows updated
+    /// </summary>>
+    public Task<long> ExecuteNonQueryAsync(string command, CRUDType crudType, Dictionary<string, object> parameters = null)
+     => _dataContext.ExecuteNonQueryAsync(command, crudType, parameters);
+
+    /// <summary>
+    /// Execute a stored procedure by given name, and return an object of type <paramref name="TEntity"/>
+    /// which satisfies the given parameter
+    /// </summary>>
+    public Task<object> ExecuteScalarAsync(string command, CRUDType crudType, string parameterName, object parameterValue)
+        => ExecuteScalarAsync(command, crudType, parameterName, parameterValue);
+
+    /// <summary>
+    /// Execute a stored procedure by given name, and return an object of type <paramref name="TEntity"/>
+    /// which satisfies the given parameters
+    /// </summary>>
+    public Task<object> ExecuteScalarAsync(string command, CRUDType crudType, Dictionary<string, object> parameters = null)
+        => _dataContext.ExecuteScalarAsync(command, crudType, parameters);
+
+    /// <summary>
+    /// Execute a stored procedure by given name, and return a collection of objects of type <paramref name="TEntity"/>
+    /// which satisfies the given parameter
+    /// </summary>>
+    public Task<IEnumerable<Dictionary<string, object>>> ExecuteReaderAsync(string command, string parameterName, object parameterValue)
+        => ExecuteReaderAsync(command, new Dictionary<string, object>() { { parameterName, parameterValue } });
+
+    /// <summary>
+    /// Execute a stored procedure by given name, and return a collection of objects of type <paramref name="TEntity"/>
+    /// which satisfies the given parameters
+    /// </summary>>
+    public Task<IEnumerable<Dictionary<string, object>>> ExecuteReaderAsync(string command, Dictionary<string, object> parameters = null)
+        => _dataContext.ExecuteReaderAsync(command, parameters);
+
+    public Task<IEnumerable<TEntity>> GetAllAsync() => _dataContext.GetAllAsync();
+
+    public Task<TEntity> GetSingleAsync(string parameterName, object parameterValue) => _dataContext.GetSingleAsync(parameterName, parameterValue);
+
+    public Task<TEntity> GetSingleAsync(long id) => _dataContext.GetSingleByIdAsync(id);
+
+    public Task<long> AddAsync(TEntity item) => _dataContext.AddAsync(item);
+
+    public Task<long> UpdateAsync(TEntity item) => _dataContext.UpdateAsync(item);
+
+    public Task<long> DeleteAsync(TEntity item) => _dataContext.DeleteAsync(item);
+  }
 }
